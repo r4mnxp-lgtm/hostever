b938c013651e45e859fed89cf4a9f4f843906152
miltonjrd <dev.miltonjrd@gmail.com>
2026-02-16 10:03:39 -0400

    env

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 const AuthContext = createContext(undefined);
 
-const API_URL = process.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: API_URL,
